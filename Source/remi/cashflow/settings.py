9c521add7719327831301ce2969d6bc5b0dedd38
Giang <Terminate2593@gmail.com>
2018-05-17 16:20:51 +0700

    settings.py

--- Source/remi/cashflow/settings.py
+++ Source/remi/cashflow/settings.py
@@ -66,9 +66,17 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATICFILES_DIRS = [
 #     # os.path.join(BASE_DIR, "static"),
 #     # os.path.join(BASE_DIR, "courses"),
-#     os.path.join(BASE_DIR, "media"),
+#     # os.path.join(BASE_DIR, "media"),
 #     os.path.join(BASE_DIR, "static")
 # ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL_RELATIVE = 'media/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -154,9 +162,4 @@ TIME_ZONE = 'UTC'
 # USE_L10N = True
 # USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
